c0ae1f5c7ea744141acd9653de2d5c1a6a19e2b6
Sergio Gomez <sergiogomez100000@gmail.com>
2021-06-30 11:31:33 -0700

    added dashboard routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,18 +1,15 @@
 // use express router
 const router = require("express").Router();
 
-// require our api routes
+// require our api, home and dashboard routes
 const apiRoutes = require("./api");
 const homeRoutes = require("./homeRoutes.js");
-// require homeRoutes
-// require dashboardRoutes --> separate from homeRoutes, if you finish the homeRoutes firs
+const dashboardRoutes = require("./dashboardRoutes.js")
 
+//use api, home, and dashboard route
 router.use("/", homeRoutes);
-router.use("/", homeRoutes);
-// router.use("dashboard", dashboardRoutes);
+router.use("dashboard", dashboardRoutes);
 router.use("/api", apiRoutes);
-router.use("/api", apiRoutes);
-
 
 // export router
 module.exports = router;
